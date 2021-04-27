ae9e9fb29fcc71772449c597f874c096c02f8509
bhavin3116 <ahir.bhavin3116@gmail.com>
2021-04-26 23:02:33 -0400

    model

---
+++ README.md
@@ -0,0 +1,1 @@
+Sample project showing car list from json file
---
+++ mobiLive_Test/Presentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mobiLive_Test
+//
+//  Created by Bhavin on 2021-04-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
