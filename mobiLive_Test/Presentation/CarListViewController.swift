d3713847eaa02067416f2cf0ce8d98cd12490ce1
bhavin3116 <ahir.bhavin3116@gmail.com>
2021-04-28 12:41:28 -0400

    Level 3 Task Added Filters

--- mobiLive_Test/Presentation/CarListViewController.swift
+++ mobiLive_Test/Presentation/CarListViewController.swift
@@ -19,85 +19,51 @@ class CarListViewController: UIViewController {
   var carDetail: [CarDetail]?
   var listTableData = [customCellData]()
   var firstIndex = true
-  
+  var isModelPicker = false
+  var isMakePicker = false
+  var modelList = [String]()
+  var makeList = [String]()
+  var modelSaveFilter = String()
+  var makerSavedFilter = String()
+  var filteredData: [CarDetail]?
+  let UIPicker: UIPickerView = UIPickerView()
+  var isFilterApplied = false
+ 
   override func viewDidLoad() {
     super.viewDidLoad()
-    tableView.delegate = self
-    tableView.dataSource = self
-    tableView.register(UINib(nibName: "CarListCell", bundle: nil), forCellReuseIdentifier: "CarListCell")
-    tableView.register(UINib(nibName: "MainViewCell", bundle: nil), forCellReuseIdentifier: "MainViewCell")
-    setTitle()
+     tableView.delegate = self
+     tableView.dataSource = self
+     tableView.register(UINib(nibName: "CarListCell", bundle: nil), forCellReuseIdentifier: "CarListCell")
+     tableView.register(UINib(nibName: "MainViewCell", bundle: nil), forCellReuseIdentifier: "MainViewCell")
+     tableView.register(UINib(nibName: "FilterCell", bundle: nil), forCellReuseIdentifier: "FilterCell")
+     setTitle()
+     UIPicker.delegate = self as UIPickerViewDelegate
+     UIPicker.dataSource = self as UIPickerViewDataSource
+     self.view.addSubview(UIPicker)
+     UIPicker.frame = CGRect(x: 0, y: self.view.bounds.size.height - UIPicker.bounds.size.height, width: self.view.bounds.size.width, height: UIPicker.bounds.size.height);
+     UIPicker.backgroundColor = UIColor.lightGray
+     UIPicker.isHidden = true
   }
-  
   override func viewWillAppear(_ animated: Bool) {
       super.viewWillAppear(animated)
       setupInitialView()
       prepareDataSource()
  }
-  
-  func setupInitialView() {
-    //Custom navigation bar
-    //navigationController?.navigationBar.prefersLargeTitles = true
-    let appearance = UINavigationBarAppearance()
-    appearance.backgroundColor = UIColor.init(red: 252.0/255.0, green: 96.0/255.0, blue: 22.0/255.0, alpha: 1.0)
-    appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
-    appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
-    navigationController?.navigationBar.tintColor = .white
-    navigationController?.navigationBar.standardAppearance = appearance
-    navigationController?.navigationBar.compactAppearance = appearance
-    navigationController?.navigationBar.scrollEdgeAppearance = appearance
-    //Set humberburger-menu class actions
-    let imageBurger = UIImage(named: "ios_burger")!
-    let btnRightMenu = UIButton(type: .system)
-    btnRightMenu.bounds = CGRect(x: 10, y: 0, width: imageBurger.size.width, height: imageBurger.size.height)
-    btnRightMenu.contentEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-    btnRightMenu.setImage(imageBurger, for: UIControl.State())
-    let rightButton = UIBarButtonItem(customView: btnRightMenu)
-    self.navigationItem.rightBarButtonItem = rightButton
-  }
-  
-  func setTitle() {
-    let TitleLabel = UILabel()
-    TitleLabel.text = "GUIDOMIA"
-    TitleLabel.textColor = .white
-    TitleLabel.font = UIFont(descriptor: UIFontDescriptor(name: "Futura-Bold", size: 25), size: 25)
-    TitleLabel.sizeToFit()
-    let leftItem = UIBarButtonItem(customView: TitleLabel)
-    self.navigationItem.leftBarButtonItem = leftItem
-  }
-  
-  func prepareDataSource() {
-    
-    // MARK: - Json fetch on main show spinner
-    if let path = Bundle.main.path(forResource: "car_list", ofType: "json") {
-        do {
-              let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
-              if let fields = try? JSONDecoder().decode([CarDetail].self, from: data) {
-              self.carDetail = fields
-            }
-          } catch {
-            // handle error
-            print(error.localizedDescription)
-          }
-       if let data = self.carDetail {
-        for carObj in data {
-          if firstIndex {
-            listTableData.append(customCellData(expanded: true, carObject: carObj))
-            firstIndex = false
-          } else {
-            listTableData.append(customCellData(expanded: false, carObject: carObj))
-           }
-          }
-        }
-    }
-  }
 }
 
 // MARK: - Table View
 extension CarListViewController: UITableViewDelegate,UITableViewDataSource {
 
    func numberOfSections(in tableView: UITableView) -> Int {
-     return carDetail!.count + 1
+    if isFilterApplied {
+      if filteredData?.count ?? 0 > 0 {
+        return filteredData!.count + 2
+      } else {
+        return 2
+      }
+    } else {
+      return carDetail!.count + 2
+    }
    }
   
    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -108,22 +74,45 @@ extension CarListViewController: UITableViewDelegate,UITableViewDataSource {
     if indexPath.section == 0 {
        return 260
     }
-    else {
-      if listTableData[indexPath.section - 1].expanded == true {
-         return UITableView.automaticDimension
-      } else {
-         return 150
-      }
+    else if indexPath.section == 1 {
+      return 200
     }
+    else {
+      if isFilterApplied {
+        if listTableData[indexPath.section - 2].expanded == true {
+           return UITableView.automaticDimension
+        } else {
+           return 150
+        }
+      } else {
+        if listTableData[indexPath.section - 2].expanded == true {
+           return UITableView.automaticDimension
+        } else {
+           return 150
+        }
+       }
+     }
    }
   
    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     if indexPath.section == 0 {
       let cell = tableView.dequeueReusableCell(withIdentifier: "MainViewCell", for: indexPath) as! MainViewCell
       return cell
-     } else {
+    } else if indexPath.section == 1 {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "FilterCell", for: indexPath) as! FilterCell
+      cell.filterDelegate = self
+      cell.makeFilter.text = makerSavedFilter
+      cell.modelFilter.text = modelSaveFilter
+      return cell
+    }
+    else {
       let cell = tableView.dequeueReusableCell(withIdentifier: "CarListCell", for: indexPath) as! CarListCell
-      let temp = carDetail![indexPath.section - 1]
+      var temp : CarDetail
+      if isFilterApplied {
+       temp =  filteredData![indexPath.section - 2]
+      } else {
+       temp = carDetail![indexPath.section - 2]
+      }
       cell.assetName?.text = temp.model
       cell.assetImage.image = UIImage(named: temp.model)
       cell.assetRatings.rating = Float(temp.rating!)
@@ -155,18 +144,170 @@ extension CarListViewController: UITableViewDelegate,UITableViewDataSource {
   
    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
   
-    if indexPath.section != 0 {
-      if listTableData[indexPath.section - 1].expanded == true {
-         listTableData[indexPath.section - 1].expanded = false
+    if indexPath.section != 0 && indexPath.section != 1 {
+      if listTableData[indexPath.section - 2].expanded == true {
+         listTableData[indexPath.section - 2].expanded = false
          let sections = IndexSet.init(integer: indexPath.section)
          tableView.reloadSections(sections, with: .automatic)
       } else {
-        listTableData[indexPath.section - 1].expanded = true
-        let sections = IndexSet.init(integer: indexPath.section)
-        tableView.reloadSections(sections, with: .automatic)
+        if isFilterApplied {
+          listTableData[indexPath.section - 2].expanded = true
+          let sections = IndexSet.init(integer: indexPath.section)
+          tableView.reloadSections(sections, with: .automatic)
+        } else {
+          listTableData[indexPath.section - 2].expanded = true
+          let sections = IndexSet.init(integer: indexPath.section)
+          tableView.reloadSections(sections, with: .automatic)
+        }
       }
     }
-    
+  }
+}
+
+
+// MARK: - Table View and Picker Views
+extension CarListViewController: filterDelegates ,UIPickerViewDelegate,UIPickerViewDataSource {
+  
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
+  }
+  func openModelFilter(_ selection: String) {
+    UIPicker.isHidden = false
+    isModelPicker = true
+    isMakePicker = false
+    UIPicker.reloadAllComponents()
+  }
+  
+  func openMakeFilter(_ selection: String) {
+    isModelPicker = false
+    isMakePicker = true
+    UIPicker.isHidden = false
+    UIPicker.reloadAllComponents()
+  }
+  
+  func closeFilter(_ selection: String) {
+    UIPicker.isHidden = true
+  }
+  func numberOfComponentsInPickerView(pickerView: UIPickerView!) -> Int{
+      return 1
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+    if isMakePicker {
+      return makeList.count
+    } else {
+      return modelList.count
+    }
+  }
+ 
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    if isMakePicker {
+      return makeList[row]
+    } else {
+      return modelList[row]
+    }
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    if isMakePicker {
+      makerSavedFilter = makeList[row]
+    } else {
+      modelSaveFilter = modelList[row]
+    }
+    prepareFilteredDataSource(Model: modelSaveFilter, Maker: makerSavedFilter)
+  }
+ 
+  func  getFilterQuearyData()  {
+    if isFilterApplied {
+      listTableData.removeAll()
+      if let data = filteredData {
+       for carObj in data {
+         if firstIndex {
+           listTableData.append(customCellData(expanded: true, carObject: carObj))
+           firstIndex = false
+         } else {
+           listTableData.append(customCellData(expanded: false, carObject: carObj))
+          }
+         }
+       }
+    } else {
+      if let data = self.carDetail {
+        listTableData.removeAll()
+       for carObj in data {
+         if firstIndex {
+           listTableData.append(customCellData(expanded: true, carObject: carObj))
+           firstIndex = false
+         } else {
+           listTableData.append(customCellData(expanded: false, carObject: carObj))
+          }
+          modelList.append(carObj.model)
+          makeList.append(carObj.make)
+         }
+       }
+    }
+  }
+}
+
+extension CarListViewController {
+  
+   func setupInitialView() {
+    //Custom navigation bar
+    //navigationController?.navigationBar.prefersLargeTitles = true
+    let appearance = UINavigationBarAppearance()
+    appearance.backgroundColor = UIColor.init(red: 252.0/255.0, green: 96.0/255.0, blue: 22.0/255.0, alpha: 1.0)
+    appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+    appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+    navigationController?.navigationBar.tintColor = .white
+    navigationController?.navigationBar.standardAppearance = appearance
+    navigationController?.navigationBar.compactAppearance = appearance
+    navigationController?.navigationBar.scrollEdgeAppearance = appearance
+    //Set humberburger-menu class actions
+    let imageBurger = UIImage(named: "ios_burger")!
+    let btnRightMenu = UIButton(type: .system)
+    btnRightMenu.bounds = CGRect(x: 10, y: 0, width: imageBurger.size.width, height: imageBurger.size.height)
+    btnRightMenu.contentEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    btnRightMenu.setImage(imageBurger, for: UIControl.State())
+    let rightButton = UIBarButtonItem(customView: btnRightMenu)
+    self.navigationItem.rightBarButtonItem = rightButton
+  }
+  
+  func setTitle() {
+    let TitleLabel = UILabel()
+    TitleLabel.text = "GUIDOMIA"
+    TitleLabel.textColor = .white
+    TitleLabel.font = UIFont(descriptor: UIFontDescriptor(name: "Futura-Bold", size: 25), size: 25)
+    TitleLabel.sizeToFit()
+    let leftItem = UIBarButtonItem(customView: TitleLabel)
+    self.navigationItem.leftBarButtonItem = leftItem
+  }
+  
+  func prepareDataSource() {
+    // MARK: - Json fetch on main show spinner
+    if let path = Bundle.main.path(forResource: "car_list", ofType: "json") {
+        do {
+              let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+              if let fields = try? JSONDecoder().decode([CarDetail].self, from: data) {
+              self.carDetail = fields
+            }
+          } catch {
+            // handle error
+            print(error.localizedDescription)
+          }
+    }
+    getFilterQuearyData()
+  }
+  
+  func prepareFilteredDataSource(Model : String , Maker : String) {
+     isFilterApplied = true
+    if Maker.count != 0 {
+      filteredData = carDetail?.filter({$0.make == Maker})
+    }
+    if Model.count != 0 {
+      filteredData = carDetail?.filter({$0.model == Model})
+    }
+    UIPicker.isHidden = true
+    getFilterQuearyData()
+    tableView.reloadData()
   }
 }
 
--- mobiLive_Test/Presentation/FilterCell.swift
+++ mobiLive_Test/Presentation/FilterCell.swift
@@ -7,19 +7,57 @@
 
 import UIKit
 
-class FilterCell: UITableViewCell {
+protocol filterDelegates: AnyObject {
+  func openModelFilter(_ selection: String)
+  func openMakeFilter(_ selection: String)
+  func closeFilter(_ selection: String)
+}
 
-  @IBOutlet weak var makeFilter: UITextField!
-  @IBOutlet weak var modelFilter: UITextField!
+class FilterCell: UITableViewCell {
+  
+  weak var filterDelegate: filterDelegates?
+  
+  @IBOutlet weak var view: UIView! {
+    didSet {
+      view.layer.cornerRadius = 10
+      view.clipsToBounds = true
+    }
+  }
+  @IBOutlet weak var makeFilter: UITextField!{
+    didSet {
+      makeFilter.delegate = self
+      makeFilter.tag = 1
+    }
+  }
+  
+  @IBOutlet weak var modelFilter: UITextField! {
+    didSet {
+      modelFilter.delegate = self
+      modelFilter.tag = 2
+    }
+  }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+  
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
-    
+  
+}
+
+extension FilterCell: UITextFieldDelegate {
+  
+  func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+    if textField.tag == 1 {
+      filterDelegate?.openMakeFilter("")
+    }
+    if textField.tag == 2 {
+      filterDelegate?.openModelFilter("")
+    }
+    return false
+  }
 }
